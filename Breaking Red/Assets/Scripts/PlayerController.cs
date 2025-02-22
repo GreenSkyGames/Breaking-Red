7151f7840bb795461a2b3fcbb111450f76930f36
Alex <asenst2004@gmail.com>
2025-02-22 06:35:57 -0800

    Beginning Level 1 Design

--- Breaking Red/Assets/Scripts/PlayerController.cs
+++ Breaking Red/Assets/Scripts/PlayerController.cs
@@ -1,7 +1,13 @@
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    [SerializeField] private float _speed = 5;
+    public int facingDirection = 1;
+    public Rigidbody2D rb;
+    public Animator anim;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -9,8 +15,26 @@ public class NewMonoBehaviourScript : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        if(horizontal > 0 && transform.localScale.x < 0 ||
+            horizontal < 0 && transform.localScale.x > 0)
+        {
+            Flip();
+        }
+
+        anim.SetFloat("horizontal", Mathf.Abs(horizontal));
+        anim.SetFloat("vertical", Mathf.Abs(vertical));
+
+        rb.linearVelocity = new Vector2(horizontal, vertical) * _speed;
+    }
+
+    void Flip()
+    {
+        facingDirection *= -1;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
     }
 }
