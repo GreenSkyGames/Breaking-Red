0bd6ec21fba09501f5794993a572105fa1c30215
drizzydunks21 <drizzyfinnadunks@gmail.com>
2025-02-24 22:37:47 -0800

    Game Updated
    Added Health Bar. Also if you touch goblin it will kill the player.

---
+++ Breaking Red Game/Assets/Scripts/HealthDisplay/HealthBarController.cs
@@ -0,0 +1,116 @@
+﻿// /*
+//  *  Author: ariel oliveira [o.arielg@gmail.com]
+//  */
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarController : MonoBehaviour
+{
+    public int health;
+    public int maxHealth;
+
+    public Sprite emptyHeart;
+    public Sprite fullHeart;
+    public Image[] hearts;
+
+    public PlayerHealth playerHealth; //allows for communication between scripts
+
+    void Update(){
+
+        health = playerHealth.currentHealth;
+        maxHealth = playerHealth.maxHealth;
+
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < health)
+            {
+                //hearts[i].gameObject.SetActive(true); // Corrected: Added parentheses
+                hearts[i].sprite = fullHeart;
+            }
+            else{
+                hearts[i].sprite = emptyHeart;
+            }
+            if (i < maxHealth)
+            {
+                hearts[i].gameObject.SetActive(true); // Corrected: Added parentheses
+                //hearts[i].sprite = emptyHeart;
+            }
+            else
+            {
+                hearts[i].gameObject.SetActive(false); // Corrected: Added parentheses
+            }
+        }
+    }
+}
+// {
+//     private GameObject[] heartContainers;
+//     private Image[] heartFills;
+
+//     public Transform heartsParent;
+//     public GameObject heartContainerPrefab;
+
+//     private void Start()
+//     {
+//         // Should I use lists? Maybe :)
+//         heartContainers = new GameObject[(int)PlayerStats.Instance.MaxTotalHealth];
+//         heartFills = new Image[(int)PlayerStats.Instance.MaxTotalHealth];
+
+//         PlayerStats.Instance.onHealthChangedCallback += UpdateHeartsHUD;
+//         InstantiateHeartContainers();
+//         UpdateHeartsHUD();
+//     }
+
+//     public void UpdateHeartsHUD()
+//     {
+//         SetHeartContainers();
+//         SetFilledHearts();
+//     }
+
+//     void SetHeartContainers()
+//     {
+//         for (int i = 0; i < heartContainers.Length; i++)
+//         {
+//             if (i < PlayerStats.Instance.MaxHealth)
+//             {
+//                 heartContainers[i].SetActive(true);
+//             }
+//             else
+//             {
+//                 heartContainers[i].SetActive(false);
+//             }
+//         }
+//     }
+
+//     void SetFilledHearts()
+//     {
+//         for (int i = 0; i < heartFills.Length; i++)
+//         {
+//             if (i < PlayerStats.Instance.Health)
+//             {
+//                 heartFills[i].fillAmount = 1;
+//             }
+//             else
+//             {
+//                 heartFills[i].fillAmount = 0;
+//             }
+//         }
+
+//         if (PlayerStats.Instance.Health % 1 != 0)
+//         {
+//             int lastPos = Mathf.FloorToInt(PlayerStats.Instance.Health);
+//             heartFills[lastPos].fillAmount = PlayerStats.Instance.Health % 1;
+//         }
+//     }
+
+//     void InstantiateHeartContainers()
+//     {
+//         for (int i = 0; i < PlayerStats.Instance.MaxTotalHealth; i++)
+//         {
+//             GameObject temp = Instantiate(heartContainerPrefab);
+//             temp.transform.SetParent(heartsParent, false);
+//             heartContainers[i] = temp;
+//             heartFills[i] = temp.transform.Find("HeartFill").GetComponent<Image>();
+//         }
+//     }
+// }
---
+++ Breaking Red Game/Assets/Scripts/HealthDisplay/PlayerHealth.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int currentHealth = 3;
+    public int maxHealth = 3;
+
+    public SpriteRenderer playerSr;
+    public PlayerController playerMovement; // Assuming this controls movement
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        currentHealth += amount;
+
+        if (currentHealth <= 0)
+        {
+            // Disable rendering and movement, but keep the object active
+            if (playerSr != null)
+            {
+                playerSr.enabled = false;
+            }
+
+            if (playerMovement != null)
+            {
+                playerMovement.enabled = false; //disable the player controller script
+            }
+
+            // Optionally, you might want to add other game over logic here,
+            // such as displaying a game over screen, triggering events, etc.
+        }
+        else
+        {
+            //if you want to re-enable them when gaining health back, add this, or similar logic.
+            if (playerSr != null && !playerSr.enabled)
+            {
+                playerSr.enabled = true;
+            }
+
+            if (playerMovement != null && !playerMovement.enabled)
+            {
+                playerMovement.enabled = true;
+            }
+        }
+    }
+
+    // Function to re-enable the player (if needed later)
+    public void ReEnablePlayer()
+    {
+        if (playerSr != null)
+        {
+            playerSr.enabled = true;
+        }
+
+        if (playerMovement != null)
+        {
+            playerMovement.enabled = true;
+        }
+        currentHealth = maxHealth;
+    }
+}
