04a966ea61f5d71f95a2d859e68a02d6d096b346
Alex <asenst2004@gmail.com>
2025-02-25 07:17:43 -0800

    Added Damaging Environment Script

---
+++ Breaking Red Game/Assets/Scripts/DamagingEnv.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class DamagingEnv : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<PlayerHealth>().ChangeHealth(-damage);
+        }
+    }
+}
