b32115c1ce04bd48a74e861ffedfe8335564eb39
Thyme-mew <tianhengyi@hotmail.com>
2025-03-08 17:54:27 -0800

    update audio manager
    update audio manager

--- Breaking Red Game/Assets/Scripts/SceneEngine/AudioManager/AudioManager.cs
+++ Breaking Red Game/Assets/Scripts/SceneEngine/AudioManager/AudioManager.cs
@@ -5,5 +5,80 @@ using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
+    public static AudioManager instance;
     public AudioType[] AudioTypes;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        foreach (var type in AudioTypes)
+        {
+            type.Source = gameObject.AddComponent<AudioSource>();
+
+            type.Source.clip = type.Clip;
+            type.Source.name = type.Name;
+            type.Source.volume = type.Volume;
+            type.Source.pitch = type.Pitch;
+            type.Source.loop = type.Loop;
+
+            if(type.Group != null)
+            {
+                type.Source.outputAudioMixerGroup = type.Group;
+            }
+        }
+    }
+
+    public void Play(string name)
+    {
+        foreach(AudioType type in AudioTypes)
+        {
+            if (type.Name == name)
+            {
+                type.Source.Play();
+                return;
+            }
+        }
+        Debug.LogWarning("Can not find audio named " + name + "!");
+    }
+
+    public void Pause(string name)
+    {
+        foreach (AudioType type in AudioTypes)
+        {
+            if (type.Name == name)
+            {
+                type.Source.Pause();
+                return;
+            }
+        }
+        Debug.LogWarning("Can not find audio named " + name + "!");
+    }
+
+    public void Stop(string name)
+    {
+        foreach (AudioType type in AudioTypes)
+        {
+            if (type.Name == name)
+            {
+                type.Source.Stop();
+                return;
+            }
+        }
+        Debug.LogWarning("Can not find audio named " + name + "!");
+    }
+
 }
