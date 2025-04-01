277553c558420f10b28f0c0ea7d8a87ddc0f9550
drizzydunks21 <drizzyfinnadunks@gmail.com>
2025-03-31 18:54:13 -0700

    Bug Issues with Red Riding Hood

--- Breaking Red Game/Assets/Scripts/BCMode.cs
+++ Breaking Red Game/Assets/Scripts/BCMode.cs
@@ -1,51 +1,51 @@
-// //BC mode script
+//BC mode script
 
-// using System.Runtime.CompilerServices;
-// using UnityEngine;
-// using UnityEngine.UI;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.UI;
 
-// public class BCMODE : MonoBehaviour
-// {
-//  [SerializeField] private Toggle _toggle;
-//  private static BCMODE _instance; 
+public class BCMODE : MonoBehaviour
+{
+ [SerializeField] private Toggle _toggle;
+ private static BCMODE _instance; 
 
-//  void Awake()
-//  {
-//     if(_instance == null){
-//         _instance = this; 
-//         DontDestroyOnLoad(_toggle); //prevent from being destroyed when scenes swtich 
-//     }else{
-//         Destroy(_toggle); //only one instance exists
-//     }
-//  }
+ void Awake()
+ {
+    if(_instance == null){
+        _instance = this; 
+        DontDestroyOnLoad(_toggle); //prevent from being destroyed when scenes swtich 
+    }else{
+        Destroy(_toggle); //only one instance exists
+    }
+ }
 
-//  void Start()
-//  {
-//     if(_toggle == null)
-//     {
-//         _toggle = GetComponent<Toggle>();
-//         if(_toggle == null)
-//         {
-//             Debug.LogError("Toggle component not found");
-//         }
-//     }
+ void Start()
+ {
+    if(_toggle == null)
+    {
+        _toggle = GetComponent<Toggle>();
+        if(_toggle == null)
+        {
+            Debug.LogError("Toggle component not found");
+        }
+    }
 
-//     bool savedState = PlayerPrefs.GetInt("BCMode", 0) == 1;
+    bool savedState = PlayerPrefs.GetInt("BCMode", 0) == 1;
 
-//     _toggle.isOn = savedState;
+    _toggle.isOn = savedState;
 
-//     _toggle.onValueChanged.AddListener(OnToggleValueChanged);
-//  }
+    _toggle.onValueChanged.AddListener(OnToggleValueChanged);
+ }
 
-// // When the BC togglge is enabled, making sure that the choice is saved for the rest of the game 
-//  public void OnToggleValueChanged(bool isON)
-//  {
-//     // Play the button click
-//     AudioManager.instance.Play("ClickSound");
+// When the BC togglge is enabled, making sure that the choice is saved for the rest of the game 
+ public void OnToggleValueChanged(bool isON)
+ {
+    // Play the button click
+    AudioManager.instance.Play("ClickSound");
 
-//     PlayerPrefs.SetInt("BCMode", isON ? 1 : 0);
-//     PlayerPrefs.Save();
-//     Debug.Log($"BC Mode toggled: {(isON ? "ON" : "Off")}"); 
-//  }
-// }
+    PlayerPrefs.SetInt("BCMode", isON ? 1 : 0);
+    PlayerPrefs.Save();
+    Debug.Log($"BC Mode toggled: {(isON ? "ON" : "Off")}"); 
+ }
+}
 
