20a709943c5748c123ad2e86e0b6a398c73b7158
Alex <asenst2004@gmail.com>
2025-04-10 19:36:15 -0700

    Attempt to fix compiler error

--- Breaking Red Game/Assets/Scripts/TL2+ Level Scripts/LevelLoader.cs
+++ Breaking Red Game/Assets/Scripts/TL2+ Level Scripts/LevelLoader.cs
@@ -8,17 +8,37 @@ public abstract class LevelLoader
     {
         Debug.Log("Default Level Loader used");
     }
+
+    public virtual string LevelID => "Base";
     public virtual LevelLoader GetNextLevel(string tag)
     {
+        return new Level1();
+    }
+
+    public virtual Vector2 GetTagPosition(string tag) {
         switch (tag)
         {
-            case "IL1": 
-                return new Level1();
-            case "L2": 
-                return new Level2();
-            default: 
-                Debug.LogWarning("Unknown level tag: " + tag); 
-                return this;
+            case "L1":
+                return new Vector2(10.5f, -8.43f);
+            case "IL1":
+                return new Vector2(-1, -1f);
+            case "IL1.1":
+                return new Vector2(-107.66f, -17.32f);
+            case "L2":
+                return new Vector2(79.71f, -24.84f);
+            case "IL2":
+                return new Vector2(181.2f, -14.6f);
+            case "L3":
+                return new Vector2(287.23f, -0.26f);
+            case "IL3":
+                return new Vector2(317.29f, -61.84f);
+            case "L4":
+                return new Vector2(221.87f, -70.49f);
+            case "L5":
+                return new Vector2(30.21f, -118.86f);
+            default:
+                Debug.LogWarning("No destination set for tag: " + tag);
+                return Vector2.zero;
         }
     }
 };
--- Breaking Red Game/Assets/Scripts/TL2+ Level Scripts/NormalPassages.cs
+++ Breaking Red Game/Assets/Scripts/TL2+ Level Scripts/NormalPassages.cs
@@ -14,7 +14,7 @@ using UnityEngine.UI;
 public class NormalPassage : TerrainObjects
 {
     public CanvasGroup fadePanel;
-    public GameObject passagePrefab;
+    public LevelLoader levelLoader;
 
     /*public NormalPassage(Vector3 pos, string sprite) : base(pos, sprite)
     {
@@ -30,7 +30,13 @@ public class NormalPassage : TerrainObjects
         {
             AudioManager.instance.Play("DoorSound"); // Play the open door sound
 
-            Vector2 newPosition = getDestination();
+            string passageTag = gameObject.tag;
+
+            LevelLoader nextLevel = levelLoader.GetNextLevel(passageTag);
+            bool isSameLevel = levelLoader.LevelID == nextLevel.LevelID;
+
+            //Vector2 newPosition = getDestination();
+            Vector2 newPosition = levelLoader.GetTagPosition(passageTag);
             if(newPosition != Vector2.zero)
             {
                 // Log the tag to check the value
@@ -42,6 +48,13 @@ public class NormalPassage : TerrainObjects
                     rb.linearVelocity = Vector2.zero;
                     rb.constraints = RigidbodyConstraints2D.FreezeAll;
                 }
+
+                if (!isSameLevel)
+                {
+                    LevelLoader loaded = LevelManager.GetOrCreateLevel(nextLevel);
+                    levelLoader = loaded;
+                }
+
                 //other.transform.position = newPosition;
                 StartCoroutine(teleportWithFade(other, newPosition, rb));
             }
@@ -99,23 +112,32 @@ public class NormalPassage : TerrainObjects
     }
     /* This function determines where a player should go based on the tag of the passageway they make contact with
      * It returns a value back to the original OnTriggerEnter2D function from above and properly changes the scenes as necessary for some levels*/
-    private Vector2 getDestination()
+    /*private Vector2 getDestination()
     {
         switch (gameObject.tag)
         {
-            case "L1": return new Vector2(10.5f, -8.43f);
-            case "IL1": return new Vector2(-1, -1f);
-            case "IL1.1": return new Vector2(-107.66f, -17.32f);
-            case "L2": return new Vector2(79.71f, -24.84f);
-            case "IL2": return new Vector2(181.2f, -14.6f);
-            case "L3": return new Vector2(287.23f, -0.26f);
-            case "IL3": return new Vector2(317.29f, -61.84f);
-            case "L4": return new Vector2(221.87f, -70.49f);
-            case "L5": return new Vector2(30.21f, -118.86f);
+            case "L1": 
+                return new Vector2(10.5f, -8.43f);
+            case "IL1": 
+                return new Vector2(-1, -1f);
+            case "IL1.1": 
+                return new Vector2(-107.66f, -17.32f);
+            case "L2": 
+                return new Vector2(79.71f, -24.84f);
+            case "IL2": 
+                return new Vector2(181.2f, -14.6f);
+            case "L3": 
+                return new Vector2(287.23f, -0.26f);
+            case "IL3": 
+                return new Vector2(317.29f, -61.84f);
+            case "L4": 
+                return new Vector2(221.87f, -70.49f);
+            case "L5": 
+                return new Vector2(30.21f, -118.86f);
             default:
                 Debug.LogWarning("No destination set for tag: " + gameObject.tag);
                 return Vector2.zero;
-        }
+        }*/
         /*switch(gameObject.tag)
         {
             case "L1":
@@ -148,6 +170,6 @@ public class NormalPassage : TerrainObjects
             default:
                 Debug.LogWarning("No destination set for tag: " + gameObject.tag);
                 return Vector2.zero;
-        }*/
-    }
+        }
+    }*/
 }
