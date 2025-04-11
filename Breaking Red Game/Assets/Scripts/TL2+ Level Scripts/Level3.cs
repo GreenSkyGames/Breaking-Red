f2ad89ba0e70cb5a3b61f5bf900fc21e46e7e31a
Alex <asenst2004@gmail.com>
2025-04-11 04:59:42 -0700

    Fix compiler error

--- Breaking Red Game/Assets/Scripts/TL2+ Level Scripts/Level3.cs
+++ Breaking Red Game/Assets/Scripts/TL2+ Level Scripts/Level3.cs
@@ -44,48 +44,7 @@ public class Level3 : LevelLoader
         AddRectTiles(L3toL4, -3, -2, 3, 4);
 
         CreatePlatform(movingPlatformPrefab, movingPlatformTilePrefab, new Vector3(285.3361f, -26.723f, 0f), L3toL4, -7f, 0f, 3f);
-
-        var L2toL4 = new System.Collections.Generic.List<Vector2>();
-        AddRectTiles(L2toL4, -3, -3, 0, 2);
-        AddRectTiles(L2toL4, -2, -1, -1, 3);
-        AddRectTiles(L2toL4, 0, 2, 0, 3);
-        AddRectTiles(L2toL4, 3, 3, 1, 3);
-
-        CreatePlatform(movingPlatformPrefab, movingPlatformTilePrefab, new Vector3(197.443f, -35.031f, 0f), L2toL4, 0f, -10f, 4f);
-
-        var I3toI4T = new System.Collections.Generic.List<Vector2>();
-        AddRectTiles(I3toI4T, -3, -1, -4, 2);
-        AddRectTiles(I3toI4T, 0, 5, -4, 0);
-        AddRectTiles(I3toI4T, 2, 5, -9, -5);
-
-        CreatePlatform(movingPlatformPrefab, movingPlatformTilePrefab, new Vector3(188.368f, 6.954f, 0f), I3toI4T, 0f, -10f, 4f);
-
-        var I1toI2 = new System.Collections.Generic.List<Vector2>();
-        AddRectTiles(I1toI2, -3, -2, -2, 2);
-        AddRectTiles(I1toI2, -1, 1, -4, 2);
-        AddRectTiles(I1toI2, 0, 2, 3, 4);
-        AddRectTiles(I1toI2, 2, 3, -1, 2);
-
-        CreatePlatform(movingPlatformPrefab, movingPlatformTilePrefab, new Vector3(89.53f, -4.06f, 0f), I1toI2, 0f, 9f, 4f);
-
-        var I2toI3 = new System.Collections.Generic.List<Vector2>();
-        AddRectTiles(I2toI3, -3, -2, -2, 2);
-        AddRectTiles(I2toI3, -1, -1, -3, 2);
-        AddRectTiles(I2toI3, 0, 3, 0, 3);
-
-        CreatePlatform(movingPlatformPrefab, movingPlatformTilePrefab, new Vector3(102.4f, 19.821f, 0f), I2toI3, 23f, 0f, 11f);
-
-        var I1toI4 = new System.Collections.Generic.List<Vector2>();
-        AddRectTiles(I1toI4, -3, 0, -5, 3);
-        AddRectTiles(I1toI4, -1, 10, 4, 5);
-        AddRectTiles(I1toI4, 1, 10, -2, 3);
-        AddRectTiles(I1toI4, 4, 7, 6, 8);
-        AddRectTiles(I1toI4, 6, 10, -4, -3);
-        AddRectTiles(I1toI4, 11, 12, -3, 5);
-
-        CreatePlatform(movingPlatformPrefab, movingPlatformTilePrefab, new Vector3(103.45f, -24.02f, 0f), I1toI4, 60f, 0f, 20f);
-
-    }
+}
 
 
 
