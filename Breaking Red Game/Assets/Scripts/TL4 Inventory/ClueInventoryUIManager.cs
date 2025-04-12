2281e69f24715af6642c8666d25cfd493dbd674e
violetspeck <swpeck2004@gmail.com>
2025-04-12 15:34:28 -0700

    Attempts to start testing

--- Breaking Red Game/Assets/Scripts/TL4 Inventory/ClueInventoryUIManager.cs
+++ Breaking Red Game/Assets/Scripts/TL4 Inventory/ClueInventoryUIManager.cs
@@ -3,18 +3,18 @@ using UnityEngine;
 public class ClueInventoryUIManager : MonoBehaviour
 {
     public GameObject[] itemSlots; // add in inspector
-    private ClueInventory clueInventory; // don't need to drag
+    private InventoryBase inventoryBase; // don't need to drag
     public int cluesGathered;
 
     void Start()
     {
-        clueInventory = new ClueInventory(); // initializw
+        inventoryBase = new ClueInventory(); // initializw
         updateSlotVisibility();
     }
 
     public void updateSlotVisibility()
     {
-        int size = clueInventory.v_getInventorySize(cluesGathered);
+        int size = inventoryBase.v_getInventorySize(cluesGathered);
 
         for (int i = 0; i < itemSlots.Length; i++)
         {
